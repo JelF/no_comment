ff637d112766d1dc051a5c4535babb2477ece758
Alexander Smirnov <begdory4@gmail.com>
2014-10-23 00:29:33 +0400

    added django_Extensions and bootstrap3

--- no_comment/settings.py
+++ no_comment/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'bootstrap3',
     'comment',
 )
 
@@ -82,3 +84,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static'
