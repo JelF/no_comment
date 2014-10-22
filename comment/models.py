3c6382270dd0885422ca743908c442a4cb860b56
Alexander Smirnov <begdory4@gmail.com>
2014-10-23 00:22:10 +0400

    added comment model

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=1000)
+    parent = models.ForeignKey('Comment', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
